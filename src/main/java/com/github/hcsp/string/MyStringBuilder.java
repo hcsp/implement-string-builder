70ff6563ca64e48730db1ec298f7c63999bc7043
Scott_Yu <51253421+Scott-YuYan@users.noreply.github.com>
2019-09-28 22:50:51 +0800

    Yyy (#3)
    Yyy (#3)

--- src/main/java/com/github/hcsp/string/MyStringBuilder.java
+++ src/main/java/com/github/hcsp/string/MyStringBuilder.java
@@ -1,24 +1,47 @@
 package com.github.hcsp.string;
 
-public class MyStringBuilder {
-    public MyStringBuilder() {}
+import java.io.UnsupportedEncodingException;
+
+public final class MyStringBuilder {
+    StringBuilder stringBuilder = new StringBuilder();
 
     // 在末尾添加一个字符
-    public MyStringBuilder append(char ch) {}
+    public MyStringBuilder append(char ch) {
+        stringBuilder.append(ch);
+        return this;
+    }
 
     // 在末尾添加一个字符串，其数据需要从bytes字节数组中按照charsetName字符集解码得到
     // 请思考一下字节和字符串（字符串本质上是字节数组）之间d关系
     // 并查找相关API
-    public MyStringBuilder append(byte[] bytes, String charsetName) {}
+    public MyStringBuilder append(byte[] bytes, String charsetName) throws UnsupportedEncodingException {
+        char[] chars = new String(bytes, charsetName).toCharArray();
+        for (char c : chars
+        ) {
+            stringBuilder.append(c);
+        }
+        return this;
+    }
 
     // 在index指定位置添加一个字符ch
-    public MyStringBuilder insert(int index, char ch) {}
+    public MyStringBuilder insert(int index, char ch) {
+        stringBuilder.insert(index, ch);
+        return this;
+    }
 
     // 删除位于index处的字符
-    public MyStringBuilder deleteCharAt(int index) {}
-
-    @Override
-    public String toString() {
-        return null;
+    public MyStringBuilder deleteCharAt(int index) {
+        stringBuilder.deleteCharAt(index);
+        return this;
     }
+
+
+    public String toString() {
+        return stringBuilder.toString();
+    }
+
 }
+
+
+
+
