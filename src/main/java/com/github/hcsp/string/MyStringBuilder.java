33aecc60aebef72c95e5978652ec2afa78ccc072
cyunt <chenyuntao1997@163.com>
2020-08-20 10:58:54 +0800

    implement a stringbuilder

--- src/main/java/com/github/hcsp/string/MyStringBuilder.java
+++ src/main/java/com/github/hcsp/string/MyStringBuilder.java
@@ -1,24 +1,41 @@
 package com.github.hcsp.string;
 
+import java.io.UnsupportedEncodingException;
+
 public class MyStringBuilder {
-    public MyStringBuilder() {}
+    StringBuilder stringBuilder = new StringBuilder();
+
+    public MyStringBuilder() {
+    }
 
     // 在末尾添加一个字符
-    public MyStringBuilder append(char ch) {}
+    public MyStringBuilder append(char ch) {
+        stringBuilder.append(ch);
+        return this;
+    }
 
     // 在末尾添加一个字符串，其数据需要从bytes字节数组中按照charsetName字符集解码得到
     // 请思考一下字节和字符串（字符串本质上是字节数组）之间d关系
     // 并查找相关API
-    public MyStringBuilder append(byte[] bytes, String charsetName) {}
+    public MyStringBuilder append(byte[] bytes, String charsetName) throws UnsupportedEncodingException {
+        stringBuilder.append(new String(bytes, charsetName));
+        return this;
+    }
 
     // 在index指定位置添加一个字符ch
-    public MyStringBuilder insert(int index, char ch) {}
+    public MyStringBuilder insert(int index, char ch) {
+        stringBuilder.insert(index, ch);
+        return this;
+    }
 
     // 删除位于index处的字符
-    public MyStringBuilder deleteCharAt(int index) {}
+    public MyStringBuilder deleteCharAt(int index) {
+        stringBuilder.deleteCharAt(index);
+        return this;
+    }
 
     @Override
     public String toString() {
-        return null;
+        return stringBuilder.toString();
     }
 }
