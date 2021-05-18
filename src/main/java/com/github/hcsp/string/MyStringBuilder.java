7bdd9fc49772173a7d2a67150bd88e9a4c937826
weiranyi <67884344+weiranyi@users.noreply.github.com>
2021-05-18 09:27:06 +0800

    Update MyStringBuilder.java

--- src/main/java/com/github/hcsp/string/MyStringBuilder.java
+++ src/main/java/com/github/hcsp/string/MyStringBuilder.java
@@ -1,24 +1,45 @@
 package com.github.hcsp.string;
 
+import java.nio.charset.Charset;
+
 public class MyStringBuilder {
-    public MyStringBuilder() {}
+    // 私有化一个StringBuilder对象，用于字符的存储
+    private StringBuilder sb;
+
+    // 在无参构造器中创建StringBuilder对象
+    public MyStringBuilder() {
+        this.sb = new StringBuilder();
+    }
 
     // 在末尾添加一个字符
-    public MyStringBuilder append(char ch) {}
+    public MyStringBuilder append(char ch) {
+        sb.append(ch);
+        return this;
+    }
 
     // 在末尾添加一个字符串，其数据需要从bytes字节数组中按照charsetName字符集解码得到
     // 请思考一下字节和字符串（字符串本质上是字节数组）之间d关系
     // 并查找相关API
-    public MyStringBuilder append(byte[] bytes, String charsetName) {}
+    public MyStringBuilder append(byte[] bytes, String charsetName) {
+        String str = new String(bytes, Charset.forName(charsetName));
+        sb.append(str);
+        return this;
+    }
 
     // 在index指定位置添加一个字符ch
-    public MyStringBuilder insert(int index, char ch) {}
+    public MyStringBuilder insert(int index, char ch) {
+        sb.insert(index, ch);
+        return this;
+    }
 
     // 删除位于index处的字符
-    public MyStringBuilder deleteCharAt(int index) {}
+    public MyStringBuilder deleteCharAt(int index) {
+        sb.deleteCharAt(index);
+        return this;
+    }
 
     @Override
     public String toString() {
-        return null;
+        return sb.toString();
     }
 }
