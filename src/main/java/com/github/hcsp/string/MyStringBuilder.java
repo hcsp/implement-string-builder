7065ef9ea2fc6695c9de48fc57f9837ebd9076c7
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-06-08 14:31:38 +0800

    采用jdk自带接口StringBuilder

--- src/main/java/com/github/hcsp/string/MyStringBuilder.java
+++ src/main/java/com/github/hcsp/string/MyStringBuilder.java
@@ -1,22 +1,18 @@
 package com.github.hcsp.string;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+import java.nio.charset.Charset;
 
 public class MyStringBuilder {
-    private char[] value;
-    private int count;
+    StringBuilder sb;
+
 
     public MyStringBuilder() {
-        int DEFAULT_CAPACITY = 16;
-        value = new char[DEFAULT_CAPACITY];
-        count = 0;
+        this.sb = new StringBuilder();
     }
 
     // 在末尾添加一个字符
     public MyStringBuilder append(char ch) {
-        ensureCapacity(count + 1);
-        value[count++] = ch;
+        sb.append(ch);
         return this;
     }
 
@@ -24,48 +20,26 @@ public class MyStringBuilder {
     // 请思考一下字节和字符串（字符串本质上是字节数组）之间d关系
     // 并查找相关API
     public MyStringBuilder append(byte[] bytes, String charsetName) {
-
-        try {
-            String appendString = new String(bytes, charsetName);
-            ensureCapacity(count + appendString.length());
-            for (int i = count; i < appendString.length() + count; i++) {
-                value[i] = appendString.charAt(i - count);
-            }
-            count += appendString.length();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        String str = new String(bytes, Charset.forName(charsetName));
+        sb.append(str);
         return this;
     }
 
     // 在index指定位置添加一个字符ch
     public MyStringBuilder insert(int index, char ch) {
-        ensureCapacity(count + 1);
-        System.arraycopy(value, index, value, index + 1, count - index);
-        value[index] = ch;
-        count += 1;
+        sb.insert(index, ch);
         return this;
     }
 
     // 删除位于index处的字符
     public MyStringBuilder deleteCharAt(int index) {
-        if (count - index >= 0) {
-            System.arraycopy(value, index + 1, value, index, count - index);
-        }
-        count--;
+        sb.deleteCharAt(index);
         return this;
     }
 
     @Override
     public String toString() {
-
-        return new String(value, 0, count);
+        return sb.toString();
     }
 
-    private void ensureCapacity(int miniCapacity) {
-        if ((miniCapacity - value.length) > 0) {
-            value = Arrays.copyOf(value,
-                    miniCapacity << 1 + 2);
-        }
-    }
 }
